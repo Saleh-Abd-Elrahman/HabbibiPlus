0f3573c42f6ddaab14dcfe14d3b1c5936237b774
farahorfaly <farah.orfaly@gmail.com>
2023-12-15 14:57:04 +0100

    Trying to fix errors for tokenizing arabic words

--- lexer.c
+++ lexer.c
@@ -4,32 +4,42 @@
 #include <stdio.h>
 
 // Function to tokenize the input
-Token *tokenize(char *source) {
+Token *tokenize(char *source) 
+{
     int capacity = 10;
     Token *tokens = malloc(capacity * sizeof(Token));
-    if (!tokens) {
+    if (!tokens) 
+    {
         fprintf(stderr, "Failed to allocate memory\n");
         exit(EXIT_FAILURE);
     }
 
     int tokenCount = 0;
 
-    while (*source != '\0') {
+    while (*source != '\0')
+    {
         // Resize tokens array if necessary
-        if (tokenCount >= capacity) {
+        if (tokenCount >= capacity) 
+        {
             capacity *= 2;
             tokens = realloc(tokens, capacity * sizeof(Token));
-            if (!tokens) {
+            if (!tokens) 
+            {
                 fprintf(stderr, "Failed to reallocate memory\n");
                 exit(EXIT_FAILURE);
             }
         }
 
-        if (isdigit(*source)) {
+        if (isdigit(*source)) 
+        {
             tokens[tokenCount].type = TOKEN_INT;
             tokens[tokenCount].intValue = strtol(source, &source, 10); // strtol advances the source pointer
-        } else {
-            switch (*source) {
+        } 
+        
+        else 
+        {
+            switch (*source) 
+            {
                 case '+': 
                     tokens[tokenCount].type = TOKEN_PLUS; 
                     break;
@@ -49,22 +59,6 @@ Token *tokenize(char *source) {
                     tokens[tokenCount].type = TOKEN_RPAREN; 
                     break;
 
-                case 'إذا': 
-                    tokens[tokenCount].type = TOKEN_IF; 
-                    break;
-
-                case 'وإلا': 
-                    tokens[tokenCount].type = TOKEN_ELSE; 
-                    break;
-
-                case 'بينما': 
-                    tokens[tokenCount].type = TOKEN_WHILE; 
-                    break;
-                
-                case 'ل': 
-                    tokens[tokenCount].type = TOKEN_FOR; 
-                    break;
-                
                 case '%': 
                     tokens[tokenCount].type = TOKEN_MODULUS; 
                     break;
@@ -161,13 +155,41 @@ Token *tokenize(char *source) {
                     tokens[tokenCount].type = TOKEN_COMMENT; 
                     break;
 
-                case 'مطبعة': 
-                    tokens[tokenCount].type = TOKEN_PRINT; 
-                    break;
-
                 default: 
-                    printf("Unexpected character: %c\n", *source);
-                    exit(1);
+                    if (strncmp(source, "إذا", strlen("إذا")) == 0) 
+                    {
+                        tokens[tokenCount].type = TOKEN_IF;
+                        break;
+                    }
+                
+                    else if (strncmp(source, "وإلا", strlen("وإلا")) == 0)
+                    {
+                        tokens[tokenCount].type = TOKEN_ELSE;
+                        break;
+                    }
+
+                    else if (strncmp(source, "بينما", strlen("بينما")) == 0)
+                    {
+                        tokens[tokenCount].type = TOKEN_WHILE;
+                        break;
+                    }
+                     
+                    else if (strncmp(source, "ل", strlen("ل")) == 0)
+                    {
+                        tokens[tokenCount].type = TOKEN_FOR;
+                        break;
+                    }
+
+                    else if (strncmp(source, "مطبعة", strlen("مطبعة")) == 0)
+                    {
+                        tokens[tokenCount].type = TOKEN_PRINT;
+                        break;
+                    }
+                    else
+                    {
+                        printf("Unexpected character: %c\n", *source);
+                        exit(1);
+                    }                  
             }
             source++;
         }
@@ -180,125 +202,165 @@ Token *tokenize(char *source) {
 }
 
 // Function to print tokens for debugging
-void printToken(Token token) {
-    switch (token.type) {
+void printToken(Token token) 
+{
+    switch (token.type) 
+    {
         case TOKEN_INT: 
             printf("INT(%d) ", token.intValue); // here
             break;
+
         case TOKEN_FLOAT:
             printf("FLOAT(%f) ", token.floatValue); // here
             break;
+
         case TOKEN_PLUS: 
             printf("PLUS "); 
             break;
+
         case TOKEN_MINUS: 
             printf("MINUS "); 
             break;
+
         case TOKEN_STAR: 
             printf("STAR "); 
             break;
         case TOKEN_SLASH: 
             printf("SLASH "); 
             break;
+
         case TOKEN_LPAREN: 
             printf("LPAREN "); 
             break;
+
         case TOKEN_RPAREN: 
             printf("RPAREN "); 
             break;
+
         case TOKEN_VARIABLE: 
             printf("VARIABLE(%s) ", token.varName); // here
             break;
+
         case TOKEN_EOF: 
             printf("EOF "); 
             break;
+
         case TOKEN_FOR: 
             printf("FOR "); 
             break;
+
         case TOKEN_IF: 
             printf("IF "); 
             break;
+
         case TOKEN_ELSE: 
             printf("ELSE "); 
             break;
+
         case TOKEN_WHILE: 
             printf("WHILE "); 
             break;
+
         case TOKEN_CHAR: 
             printf("CHAR(%c) ", token.varName); // here
             break;
+
         case TOKEN_RETURN: 
             printf("RETURN "); 
             break;
+
         case TOKEN_MODULUS: 
             printf("MODULUS "); 
             break;
+
         case TOKEN_EQUAL_TO: 
             printf("EQUAL_TO "); 
             break;
+
         case TOKEN_LESS_THAN: 
             printf("LESS_THAN "); 
             break;
+
         case TOKEN_GREATER_THAN: 
             printf("GREATER_THAN "); 
             break;
+
         case TOKEN_AND: 
             printf("AND "); 
             break;
+
         case TOKEN_OR: 
             printf("OR "); 
             break;
+            
         case TOKEN_DOUBLE_QUOTE:
             printf("DOUBLE_QUOTE ");
             break;
+
         case TOKEN_INCREMENT_BY:
             printf("INCREMENT_BY ");
             break;
+
         case TOKEN_MULTIPLY_BY:
             printf("MULTIPLY_BY ");
             break;
+
         case TOKEN_DECREASE_BY:
             printf("DECREASE_BY ");
             break;
+
         case TOKEN_DIVIDE_BY:
             printf("DIVIDE_BY ");
             break;
+
         case TOKEN_MOD_BY:
             printf("MOD_BY ");
             break;
+
         case TOKEN_NOT_EQUAL_TO:
             printf("NOT_EQUAL_TO ");
             break;
+
         case TOKEN_LESS_THAN_OR_EQUAL_TO:
             printf("LESS_THAN_OR_EQUAL_TO ");
             break;
+
         case TOKEN_GREATER_THAN_OR_EQUAL_TO:
             printf("GREATER_THAN_OR_EQUAL_TO ");
             break;
+
         case TOKEN_COMMA:
             printf("COMMA ");
             break;
+
         case TOKEN_SEMICOLON:
             printf("SEMICOLON ");
             break;
+
         case TOKEN_PERIOD:
             printf("PERIOD ");
             break;
+
         case TOKEN_COLON:
             printf("COLON ");
             break;
+
         case TOKEN_QUESTION_MARK:
             printf("QUESTION_MARK ");
             break;
+
         case TOKEN_EXCLAMATION_MARK:
             printf("EXCLAMATION_MARK ");
             break;
+
         case TOKEN_LEFT_BRACKET:
             printf("LEFT_BRACKET ");
             break;
+
         case TOKEN_RIGHT_BRACKET:
             printf("RIGHT_BRACKET ");
             break;
+            
         default:
             printf("UNKNOWN ");
             break;
